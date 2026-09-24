33461be493dfccc93535cbab19b03e50f062cfd1
k4ijin47-rgb <k4ijin47@icloud.com>
2026-09-24 14:51:51 +0900

    Initialize Discord bot with basic setup

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("OASIS Derby Bot");
+ const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = process.env.TOKEN;
+const DB = "./data.json";
+
+if (!fs.existsSync(DB)) fs.writeFileSync(DB, "{}");
+
+const load = () => JSON.parse(fs.readFileSync(DB));
+const save = d => fs.writeFileSync(DB, JSON.stringify(d));
+
+const horses = [
+  "サンダー","ルナ","フェニックス","ミスト",
+  "オアシス","ギャラクシー","ブレイズ","エクリプス"
+];
+
+client.once("ready", () => {
+  console.log("🏇 OASIS Derby 起動");
+});
+
+client.login(TOKEN);
